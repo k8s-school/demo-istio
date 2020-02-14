9ed13be463b622bff314a2714fc77cfc8de4e042
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-02-14 22:44:05 +0100

    upd

--- ci.sh
+++ ci.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 
-$DIR/istio-install.sh
-$DIR/bookinfo-install.sh
-$DIR/bookinfo-query.sh
-$DIR/port-forward.sh
-$DIR/istio-uninstall.sh
+"$DIR"/istio-install.sh
+"$DIR"/bookinfo-install.sh
+timeout 5 "$DIR"/bookinfo-query.sh
+"$DIR"/port-forward.sh
+"$DIR"/istio-uninstall.sh
