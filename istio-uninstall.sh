298c435a59dd433873730d761beea258896b2184
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-09-30 01:53:39 +0200

    upd

--- istio-uninstall.sh
+++ istio-uninstall.sh
@@ -8,6 +8,6 @@ set -euxo pipefail
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 . "$DIR"/env.sh
 
-kubectl delete -f "$ISTIO_DIR"/samples/addons
+kubectl delete -f "$ISTIO_DIR"/samples/addons --ignore-not-found=true
 istioctl manifest generate --set profile=demo | kubectl delete --ignore-not-found=true -f -
 kubectl delete namespace istio-system
